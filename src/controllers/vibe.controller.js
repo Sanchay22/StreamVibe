d4e9b33b601092225aa4b8fde936ee076ee47ff4
Sanchay Jadon <jadonsanchay@gmail.com>
2024-07-07 21:11:10 +0530

    chore: Add createVibe API endpoint and controller function

--- src/controllers/vibe.controller.js
+++ src/controllers/vibe.controller.js
@@ -7,10 +7,24 @@ import {asyncHandler} from "../utils/asyncHandler.js"
 
 const createVibe = asyncHandler(async (req, res) => {
     //TODO: create vibe
+    const post=req.body.post;
+    if(!content){
+        throw new ApiError(400,"No content");
+    }const user=req.user._id; 
+    if(!user){
+        throw new ApiError(500,"User detail can't be fetched");
+    }const vibe=await Vibe.create({
+        content:post,
+        owner:user
+    })
+    return res.status(200)
+    .json(new ApiResponse(201,vibe,"Vibe created successfully"));
+
 })
 
 const getUserVibes = asyncHandler(async (req, res) => {
     // TODO: get user vibes
+    
 })
 
 const updateVibe = asyncHandler(async (req, res) => {
